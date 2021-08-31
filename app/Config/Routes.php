5bb54f3b3040eb0b91fafa4dc52e8acf4cc727e8
Adi Kurniawan <adikurniawan.dev@gmail.com>
2021-08-31 15:52:28 +0700

    Update Routes.php

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -7,8 +7,7 @@ $routes = Services::routes();
 
 // Load the system's routing file first, so that the app and ENVIRONMENT
 // can override as needed.
-if (file_exists(SYSTEMPATH . 'Config/Routes.php'))
-{
+if (file_exists(SYSTEMPATH . 'Config/Routes.php')) {
 	require SYSTEMPATH . 'Config/Routes.php';
 }
 
@@ -33,7 +32,25 @@ $routes->setAutoRoute(true);
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
 $routes->get('/', 'Home::index');
+$routes->get('/Admin/Fakultas', 'Admin/Fakultas::index');
+$routes->get('/Admin/Fakultas/(:any)', 'Admin/Fakultas::index');
+$routes->post('/Admin/Fakultas', 'Admin/Fakultas::addFakultas');
 
+$routes->get('/Admin/Jurusan', 'Admin/Jurusan::index');
+$routes->get('/Admin/Jurusan/(:any)', 'Admin/Jurusan::index');
+$routes->post('/Admin/Jurusan', 'Admin/Jurusan::addJurusan');
+
+$routes->get('/Admin/Matakuliah', 'Admin/Matakuliah::index');
+$routes->get('/Admin/Matakuliah/(:any)', 'Admin/Matakuliah::index');
+$routes->post('/Admin/Matakuliah', 'Admin/Matakuliah::addMatakuliah');
+
+$routes->get('/Admin/Dosen', 'Admin/Dosen::index');
+$routes->get('/Admin/Dosen/(:any)', 'Admin/Dosen::index');
+$routes->post('/Admin/Dosen', 'Admin/Dosen::addDosen');
+
+$routes->get('/Admin/Mahasiswa', 'Admin/Mahasiswa::index');
+$routes->get('/Admin/Mahasiswa/(:any)', 'Admin/Mahasiswa::index');
+$routes->post('/Admin/Mahasiswa', 'Admin/Mahasiswa::addMahasiswa');
 /*
  * --------------------------------------------------------------------
  * Additional Routing
@@ -47,7 +64,6 @@ $routes->get('/', 'Home::index');
  * You will have access to the $routes object within that file without
  * needing to reload it.
  */
-if (file_exists(APPPATH . 'Config/' . ENVIRONMENT . '/Routes.php'))
-{
+if (file_exists(APPPATH . 'Config/' . ENVIRONMENT . '/Routes.php')) {
 	require APPPATH . 'Config/' . ENVIRONMENT . '/Routes.php';
 }
