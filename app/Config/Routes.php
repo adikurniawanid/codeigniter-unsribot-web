c9be4d4b814909aa8e9d5e64e0933bb2a999ad61
Adi Kurniawan <adikurniawan.dev@gmail.com>
2021-09-02 15:57:36 +0700

    Update Routes.php

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -39,18 +39,28 @@ $routes->post('/Admin/Fakultas', 'Admin/Fakultas::addFakultas');
 $routes->get('/Admin/Jurusan', 'Admin/Jurusan::index');
 $routes->get('/Admin/Jurusan/(:any)', 'Admin/Jurusan::index');
 $routes->post('/Admin/Jurusan', 'Admin/Jurusan::addJurusan');
+$routes->delete('/Admin/Jurusan/(:any)', 'Admin/Jurusan::deleteJurusan/$1');
+$routes->put('/Admin/Jurusan/(:any)', 'Admin/Jurusan::editJurusan/$1');
 
 $routes->get('/Admin/Matakuliah', 'Admin/Matakuliah::index');
 $routes->get('/Admin/Matakuliah/(:any)', 'Admin/Matakuliah::index');
 $routes->post('/Admin/Matakuliah', 'Admin/Matakuliah::addMatakuliah');
+$routes->delete('/Admin/Matakuliah/(:any)', 'Admin/Matakuliah::deleteMatakuliah/$1');
+$routes->put('/Admin/Matakuliah/(:any)', 'Admin/Matakuliah::editMatakuliah/$1');
 
 $routes->get('/Admin/Dosen', 'Admin/Dosen::index');
 $routes->get('/Admin/Dosen/(:any)', 'Admin/Dosen::index');
 $routes->post('/Admin/Dosen', 'Admin/Dosen::addDosen');
+$routes->delete('/Admin/Dosen/(:any)', 'Admin/Dosen::deleteDosen/$1');
+$routes->put('/Admin/Dosen/(:any)', 'Admin/Dosen::editDosen/$1');
 
 $routes->get('/Admin/Mahasiswa', 'Admin/Mahasiswa::index');
 $routes->get('/Admin/Mahasiswa/(:any)', 'Admin/Mahasiswa::index');
 $routes->post('/Admin/Mahasiswa', 'Admin/Mahasiswa::addMahasiswa');
+$routes->delete('/Admin/Mahasiswa/(:any)', 'Admin/Mahasiswa::deleteMahasiswa/$1');
+$routes->put('/Admin/Mahasiswa/(:any)', 'Admin/Mahasiswa::editMahasiswa/$1');
+
+$routes->post('/Admin/QueryData', 'Admin/QueryData::prosesQuery');
 /*
  * --------------------------------------------------------------------
  * Additional Routing
