5d7aedd638f45d9ab29147eb35ec414cfb542774
Adi Kurniawan <adikurniawan.dev@gmail.com>
2021-09-25 08:00:05 +0700

    Update Processing.py

--- app/PyCode/Processing.py
+++ app/PyCode/Processing.py
@@ -12,14 +12,17 @@ class database:
 def isPerintah(token):
     kalimatPerintah = ["tampil", "apa", "berapa",
                        "siapa", "cari", "lihat", "temu"]
-
+    indeks = 0
     for w in token:
         if w in kalimatPerintah:
-            result = True
+            result = w
+            indeks = indeks
             break
         else:
             result = False
-    return result
+            indeks = -1
+        indeks += 1
+    return result, indeks
 
 
 def indetifikasiTabel(token):
@@ -29,12 +32,18 @@ def indetifikasiTabel(token):
     daftarTabel = hstack(daftarTabel)
 
     teridentifikasi = []
+    indeksTeridentifikasi = []
 
+    indeks = 0
     for w in token:
         if w in daftarTabel:
             teridentifikasi.append(w)
+            indeksTeridentifikasi.append(indeks)
+            indeks += 1
+        else:
+            indeks += 1
 
-    return teridentifikasi
+    return teridentifikasi, indeksTeridentifikasi
 
 
 def indetifikasiKolom(token):
@@ -43,9 +52,52 @@ def indetifikasiKolom(token):
     daftarKolom = hstack(daftarKolom)
 
     teridentifikasi = []
+    indeksTeridentifikasi = []
 
+    indeks = 0
     for w in token:
         if w in daftarKolom:
             teridentifikasi.append(w)
+            indeksTeridentifikasi.append(indeks)
+            indeks += 1
+        else:
+            indeks += 1
+    return teridentifikasi, indeksTeridentifikasi
 
-    return teridentifikasi
+
+def indetifikasiKondisi(token):
+    daftarKondisi = ["yang", "dimana"]
+
+    teridentifikasi = []
+    indeksTeridentifikasi = []
+
+    indeks = 0
+    for w in token:
+        if w in daftarKondisi:
+            teridentifikasi.append(w)
+            indeksTeridentifikasi.append(indeks)
+            indeks += 1
+        else:
+            indeks += 1
+    return teridentifikasi, indeksTeridentifikasi
+
+
+def indetifikasiOperatorLogika(token):
+    teridentifikasi = []
+    indeksTeridentifikasi = []
+
+    indeks = 0
+    for w in token:
+        if w == "atau":
+            teridentifikasi.append(w)
+            # teridentifikasi.append("or")
+            indeksTeridentifikasi.append(indeks)
+            indeks += 1
+        elif w == "dan":
+            teridentifikasi.append(w)
+            # teridentifikasi.append("and")
+            indeksTeridentifikasi.append(indeks)
+            indeks += 1
+        else:
+            indeks += 1
+    return teridentifikasi, indeksTeridentifikasi
