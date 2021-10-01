a8c4012cd262f325749311a675c2aa0c5ca4d0b0
Adi Kurniawan <adikurniawan.dev@gmail.com>
2021-10-01 13:57:01 +0700

    Update Processing.py

--- app/PyCode/Processing.py
+++ app/PyCode/Processing.py
@@ -1,17 +1,8 @@
-from Preprocessing import tokenizing
-from Model import querySQL
-from numpy import hstack
-
-
-class database:
-    __daftarTabel = hstack(querySQL(
-        "SELECT TABLE_NAME FROM information_schema.`TABLES` WHERE TABLE_TYPE LIKE 'VIEW' AND TABLE_SCHEMA LIKE 'simak_simulasi'"))
-    __daftarKolom = hstack(querySQL("SELECT col.column_name FROM information_schema.columns col JOIN information_schema.views vie ON vie.table_schema=col.table_schema AND vie.table_name=col.table_name where col.table_schema not in ('sys', 'information_schema','mysql', 'performance_schema') AND vie.table_schema='simak_simulasi'"))
+from WordList import getDaftarTable, getDaftarPerintah, getDaftarKolom, getDaftarKondisi
 
 
 def isPerintah(token):
-    kalimatPerintah = ["tampil", "apa", "berapa",
-                       "siapa", "cari", "lihat", "temu"]
+    kalimatPerintah = getDaftarPerintah()
     indeks = 0
     for w in token:
         if w in kalimatPerintah:
@@ -25,10 +16,7 @@ def isPerintah(token):
 
 
 def indetifikasiTabel(token):
-    daftarTabel = querySQL(
-        "SELECT TABLE_NAME FROM information_schema.`TABLES` WHERE TABLE_TYPE LIKE 'VIEW' AND TABLE_SCHEMA LIKE 'simak_simulasi'")
-
-    daftarTabel = hstack(daftarTabel)
+    daftarTabel = getDaftarTable()
 
     teridentifikasi = []
     indeksTeridentifikasi = []
@@ -46,9 +34,7 @@ def indetifikasiTabel(token):
 
 
 def indetifikasiKolom(token):
-    daftarKolom = querySQL("SELECT col.column_name FROM information_schema.columns col JOIN information_schema.views vie ON vie.table_schema=col.table_schema AND vie.table_name=col.table_name where col.table_schema not in ('sys', 'information_schema','mysql', 'performance_schema') AND vie.table_schema='simak_simulasi'")
-
-    daftarKolom = hstack(daftarKolom)
+    daftarKolom = getDaftarKolom()
 
     teridentifikasi = []
     indeksTeridentifikasi = []
@@ -65,7 +51,7 @@ def indetifikasiKolom(token):
 
 
 def indetifikasiKondisi(token):
-    daftarKondisi = ["yang", "dimana"]
+    daftarKondisi = getDaftarKondisi()
 
     teridentifikasi = []
     indeksTeridentifikasi = []
