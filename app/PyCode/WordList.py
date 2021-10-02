58f53339e71186c68aba20d86a301e30e2b2b903
Adi Kurniawan <adikurniawan.dev@gmail.com>
2021-10-02 11:10:32 +0700

    Update WordList.py
    add daftarTabel dan daftarKolom untuk versi table

--- app/PyCode/WordList.py
+++ app/PyCode/WordList.py
@@ -3,16 +3,26 @@ from numpy import hstack
 
 
 class wordList:
+    __databaseName = "simak_simulasi"
+
+    # dari tabel
+    # _daftarTabel = hstack(querySQL(
+    #     f"SELECT TABLE_NAME FROM information_schema.`TABLES` WHERE  TABLE_SCHEMA LIKE '{__databaseName}'"))
+    # _daftarKolom = hstack(querySQL(
+    #     f"SELECT `COLUMN_NAME` FROM `INFORMATION_SCHEMA`.`COLUMNS` WHERE `TABLE_SCHEMA`='{__databaseName}'"))
+
+    # dari view
     _daftarTabel = hstack(querySQL(
-        "SELECT TABLE_NAME FROM information_schema.`TABLES` WHERE TABLE_TYPE LIKE 'VIEW' AND TABLE_SCHEMA LIKE 'simak_simulasi'"))
-    _daftarKolom = hstack(querySQL("SELECT col.column_name FROM information_schema.columns col JOIN information_schema.views vie ON vie.table_schema=col.table_schema AND vie.table_name=col.table_name where col.table_schema not in ('sys', 'information_schema','mysql', 'performance_schema') AND vie.table_schema='simak_simulasi'"))
-    _daftarPerintah = ["tampil", "apa", "berapa",
-                       "siapa", "cari", "lihat", "temu"]
-    _daftarKondisi = ["yang", "dimana"]
-    _daftarStopword = ["coba", "tolong"]
-    _daftarSimbol = "[@_!#$%^&*()<>?/\|}{~:]"
+        f"SELECT TABLE_NAME FROM information_schema.`TABLES` WHERE TABLE_TYPE LIKE 'VIEW' AND TABLE_SCHEMA LIKE '{__databaseName}'"))
+    _daftarKolom = hstack(querySQL(
+        f"SELECT col.column_name FROM information_schema.columns col JOIN information_schema.views vie ON vie.table_schema=col.table_schema AND vie.table_name=col.table_name where col.table_schema not in ('sys', 'information_schema','mysql', 'performance_schema') AND vie.table_schema='{__databaseName}'"))
     _daftarRelasi = querySQL(
         "SELECT `TABLE_NAME`, `COLUMN_NAME`, `REFERENCED_TABLE_SCHEMA`,`REFERENCED_TABLE_NAME`,`REFERENCED_COLUMN_NAME` FROM `INFORMATION_SCHEMA`.`KEY_COLUMN_USAGE` WHERE`TABLE_SCHEMA`=SCHEMA() AND `REFERENCED_TABLE_NAME` IS NOT NULL")
+    _daftarPerintah = ["tampil", "apa", "berapa",
+                       "siapa", "cari", "lihat", "temu"]
+    _daftarKondisi = ["yang", "dimana", "dengan"]
+    _daftarStopword = ["coba", "tolong", "milik"]
+    _daftarSimbol = "[@_!#$%^&*()<>?/\|}{~:]"
 
 
 def getDaftarPerintah():
